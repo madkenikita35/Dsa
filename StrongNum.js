940f16bb87dc53efeea945ee517eba52f92cdc02
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-24 11:52:11 +0530

    Add Final Corrected Code of Strong Number

--- StrongNum.js
+++ StrongNum.js
@@ -1,15 +1,23 @@
-let num = 123;
+let num = 145;
 let sum = 0;
-let fact = 1;
+ var copy = num;
+
 while(num > 0){
-    
+   
     let lastNum = num % 10;
+    let fact = 1;
 
     for(let i = 1; i <= lastNum; i++){
          fact = (fact * i);      
     }
     sum = sum + fact;
     num = Math.floor(num / 10)
+   
 }
-console.log("Factorial is :",fact)
-console.log("Sum of Fact is" ,sum)
+
+if(sum === copy){
+    console.log("Strong Number")
+}else{
+    console.log("Not a Strong Number")
+}
+
