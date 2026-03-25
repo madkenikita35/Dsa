321ea91e6d733b2a6816eadedc803f408fcc0b5c
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-25 11:06:54 +0530

    Add V pattern Two Ways

---
+++ Pattern/VPattern.js
@@ -0,0 +1,29 @@
+// One Way
+
+// let n = 9;
+
+// for(let i = 1; i <= Math.ceil(n/2 ) ; i++){
+//         for(let j = 1; j <= n ; j++){
+//             if(j == i || j == n - i + 1 ){
+//                 process.stdout.write("*");
+//             }else{
+//                 process.stdout.write("-");
+//             }
+// }
+// console.log()
+// }
+
+//Second Way
+
+let n = 5;
+
+for(let i = 1; i <= n ; i++){
+        for(let j = 1; j <= n*2 ; j++){
+            if(j == i || j == n * 2 - i  ){
+                process.stdout.write("*");
+            }else{
+                process.stdout.write("-");
+            }
+}
+console.log()
+}
--- Pattern/Xpattern.js
+++ Pattern/Xpattern.js
@@ -1,4 +1,4 @@
-let n = 5;
+let n = 9;
 
 for(let i = 1; i <= n ; i++){
         for(let j = 1; j <= n ; j++){
