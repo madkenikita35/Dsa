8187e68b3422aebe4af5f182d77074843698b9ae
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-25 10:03:34 +0530

    Add Pattern Codes

--- Pattern/RightAngleTriNum.js
+++ Pattern/RightAngleTriNum.js
@@ -5,3 +5,23 @@ for(var i = 1; i <= n; i++){
 }
 console.log()
 }
+//Output
+// 1
+// 12
+// 123
+// 1234
+
+//Second Way
+// let n = 4;
+// for(var i = 1; i <= n; i++){
+//     for(var j = 1; j <= i; j++){
+//         process.stdout.write(""+ i)
+// }
+// console.log()
+// }
+
+//Output
+// 1
+// 22
+// 333
+// 4444
---
+++ Pattern/Triangle.js
@@ -0,0 +1,12 @@
+let n = 4;
+
+for(let i = 1; i <= n ; i++){
+    for(let j = 1; j <= n -i ; j++){
+
+        process.stdout.write("- ")
+}
+  for(let j = 1; j <= i; j++){
+        process.stdout.write("*   ")
+}
+console.log()
+}
